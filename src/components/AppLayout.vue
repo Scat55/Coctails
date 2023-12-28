6cb279b194006780f64bb4177efb245d013a4ff2
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-28 17:35:16 +0300

    feat: added the layout of the main page

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+interface Props {
+	imageUrl: string;
+}
+
+const props = defineProps<Props>();
+const { imageUrl } = props;
+</script>
+
+<template>
+	<div class="root">
+		<div :style="`background-image: url(${imageUrl})`" class="img"></div>
+		<div class="main">
+			<el-button class="btn">Get random cocktail</el-button>
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="sass">
+@import '../assets/styles/main.sass'
+.root
+	display: flex
+	background-color: $background-color
+	min-height: 100vh
+
+.img
+	width: 50%
+	background-repeat: no-repeat
+	background-position: 50% 50%
+	background-size: cover
+
+.main
+	position: relative
+	width: 50%
+	padding: 2rem 2.5rem
+
+.btn
+	font-family: 'Raleway'
+	position: absolute
+	top: 2rem
+	right: 2.5rem
+	background-color: $accent_color
+	color: $text_color
+	font-size: 1rem
+	border-color: $accent_color
+	transition: all .3s
+
+	&:hover,
+	&:active
+		background-color: darken($accent_color, 10%)
+		border-color: darken($accent_color, 10%)
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppLayout from '@/components/AppLayout.vue';
+</script>
 
 <template>
-	<div>Home</div>
+	<AppLayout imageUrl="/src/assets/images/bg-1.jpg">
+		<div class="wrapper">
+			<div class="info">
+				<div class="title">Choose your drink</div>
+				<div class="line"></div>
+			</div>
+		</div>
+	</AppLayout>
 </template>
 
 <style scoped lang="sass">
 @import '@/assets/styles/main.sass'
+
+.wrapper
+	display: flex
+	align-items: center
+	justify-content: center
+
+.info
+	padding: 5rem 0
+	text-align: center
 </style>
